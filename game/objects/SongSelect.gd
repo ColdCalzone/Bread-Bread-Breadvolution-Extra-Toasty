b9099eaac828581c93f9f74de71f27dfca882094
ColdCalzone <coldhotpocket1@gmail.com>
2022-02-15 02:55:49 -0500

    Fix more overlapping, improve song selector

--- game/objects/SongSelect.gd
+++ game/objects/SongSelect.gd
@@ -6,10 +6,10 @@ func _ready():
 	if get_parent() is Control:
 		self.rect_min_size.x = get_parent().rect_size.x
 
-func add_song(button):
+func add_song(button : SongButton):
 	vbox.add_child(button)
-	if button is Control and self.rect_min_size.y < 360:
-		var extra_space = button.rect_min_size.y + 4 # Vbox seperation 
+	if self.rect_min_size.y < 360:
+		var extra_space = button.rect_size.y + 4 # Vbox seperation 
 		if extra_space + self.rect_min_size.y < 360:
 			self.rect_min_size.y += extra_space
 		else:
